3f05927329a54cca4b6d02fb0a426741a2435b99
tareksander <57038324+tareksander@users.noreply.github.com>
2023-06-15 19:25:53 +0200

    Init repo.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flutt.IP
+
+A cross-platform Stud.IP app build with Flutter.
+
+
+
+
+
---
+++ android/app/src/main/kotlin/com/tsander/flutt_ip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsander.flutt_ip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
